5ee32cf56e12dc2b0c131f946c23a55d2cbac6c0
MartinCalvete <63985377+MartinCalvete@users.noreply.github.com>
2020-05-10 15:42:21 -0300

    Add files via upload

---
+++ veterinaria/veterinaria/Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Veterinaria
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtRaza = New System.Windows.Forms.TextBox()
+        Me.txtColor = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.grdPerros = New System.Windows.Forms.DataGridView()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        CType(Me.grdPerros, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(55, 12)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(51, 20)
+        Me.txtID.TabIndex = 0
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(55, 49)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 1
+        '
+        'txtRaza
+        '
+        Me.txtRaza.Location = New System.Drawing.Point(55, 84)
+        Me.txtRaza.Name = "txtRaza"
+        Me.txtRaza.Size = New System.Drawing.Size(100, 20)
+        Me.txtRaza.TabIndex = 2
+        '
+        'txtColor
+        '
+        Me.txtColor.Location = New System.Drawing.Point(55, 122)
+        Me.txtColor.Name = "txtColor"
+        Me.txtColor.Size = New System.Drawing.Size(100, 20)
+        Me.txtColor.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Raza"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 129)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Color"
+        '
+        'grdPerros
+        '
+        Me.grdPerros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdPerros.Location = New System.Drawing.Point(192, 12)
+        Me.grdPerros.Name = "grdPerros"
+        Me.grdPerros.Size = New System.Drawing.Size(360, 203)
+        Me.grdPerros.TabIndex = 8
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(31, 245)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAgregar.TabIndex = 9
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Location = New System.Drawing.Point(154, 245)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEditar.TabIndex = 10
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(275, 245)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 11
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'Veterinaria
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(588, 282)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.grdPerros)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtColor)
+        Me.Controls.Add(Me.txtRaza)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtID)
+        Me.Name = "Veterinaria"
+        Me.Text = "Veterinaria"
+        CType(Me.grdPerros, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtRaza As TextBox
+    Friend WithEvents txtColor As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents grdPerros As DataGridView
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnEliminar As Button
+End Class
---
+++ veterinaria/veterinaria/Form1.vb
@@ -0,0 +1,141 @@
+﻿Imports MySql.Data.MySqlClient
+'Martin Calvete 3BF
+Public Class Veterinaria
+    Dim conexion As MySqlConnection = New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Sub ActualizarSelect()
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "SELECT * FROM perros ORDER BY nombre ASC"
+            adaptador.SelectCommand = cmd
+            adaptador.Fill(ds, "Tabla")
+            grdPerros.DataSource = ds
+            grdPerros.DataMember = "Tabla"
+
+            conexion.Close()
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "INSERT INTO perros(nombre, raza, color) VALUES(@nombre, @raza, @color)"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+            cmd.Parameters.AddWithValue("@raza", txtRaza.Text)
+            cmd.Parameters.AddWithValue("@color", txtColor.Text)
+            cmd.ExecuteNonQuery()
+            txtNombre.Clear()
+            txtRaza.Clear()
+            txtColor.Clear()
+
+            conexion.Close()
+
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+
+    Private Sub Veteriaria_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ActualizarSelect()
+    End Sub
+
+    Private Sub grdPerros_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles grdPerros.CellContentClick
+
+    End Sub
+
+    Private Sub grdPerros_SelectionChanged(sender As Object, e As EventArgs) Handles grdPerros.SelectionChanged
+        If (grdPerros.SelectedRows.Count > 0) Then
+            txtNombre.Text = grdPerros.Item("nombre", grdPerros.SelectedRows(0).Index).Value
+            txtRaza.Text = grdPerros.Item("raza", grdPerros.SelectedRows(0).Index).Value
+            txtColor.Text = grdPerros.Item("color", grdPerros.SelectedRows(0).Index).Value
+            txtID.Text = grdPerros.Item("id", grdPerros.SelectedRows(0).Index).Value
+        End If
+
+    End Sub
+
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "UPDATE perros SET nombre=@nombre, raza=@raza, color=@color WHERE id=@id"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+            cmd.Parameters.AddWithValue("@raza", txtRaza.Text)
+            cmd.Parameters.AddWithValue("@color", txtColor.Text)
+            cmd.Parameters.AddWithValue("@id", txtID.Text)
+            cmd.ExecuteNonQuery()
+            txtNombre.Clear()
+            txtRaza.Clear()
+            txtColor.Clear()
+            txtID.Clear()
+
+            conexion.Close()
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "DELETE FROM perros WHERE id=@id"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+            cmd.Parameters.AddWithValue("@raza", txtRaza.Text)
+            cmd.Parameters.AddWithValue("@color", txtColor.Text)
+            cmd.Parameters.AddWithValue("@id", txtID.Text)
+            cmd.ExecuteNonQuery()
+            txtNombre.Clear()
+            txtRaza.Clear()
+            txtColor.Clear()
+            txtID.Clear()
+
+            conexion.Close()
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+End Class
